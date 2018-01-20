767a05ce5380ca48601a0a0ed85d5552d77f9ced
Rohit Ramesh <rohitramesh47@gmail.com>
2018-01-21 02:45:52 +0530

    Correct file names

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-var events =  require('events');
-var util = require('util');
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
 
-var Person = function(name){
-	this.name = name;
-};
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
 
-util.inherits(Person, events.EventEmitter);
 
-var james = new Person('james');
-var mary = new Person('mary');
-var ryu = new Person('ryu');
 
-var people = [james, mary, ryu];
-
-	
-people.forEach(function(person){
-	person.on('speak', function(mssg){
-		console.log(person.name+" said: "+mssg);
-	});
+app.get('/', function(req, res){
+	res.render('index');
 });
 
-james.emit('speak', 'Hi there!');
+app.get('/contact', function(req, res){
+	res.render('contact', {qs: req.query}); //This is how you obtain queries from website's url
+});
+
+app.post('/contact', urlencodedParser, function(req, res){ //urlencodedParser is a middleware that gives the inputs from form as attributes of an object to be used.
+	console.log(req.body);
+	res.render('contact-success', {data: req.body});
+});
+
+app.get('/profile/:name', function(req,res){
+	var data = {name: 'ramesh', age: 19, hobbies: ['cycling', 'fighting', 'acting']};
+	res.render('profile.ejs', {person: req.params.name, data: data});
+});
+
+app.listen(3000);
+console.log("Listening to port 3000!")
